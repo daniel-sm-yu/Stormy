2b958c86bcde1884fbec5f8a206f6b46f777af8f
Daniel Yu <daniel.sm.yu@gmail.com>
2019-03-12 15:47:43 -0400

    initial commit

---
+++ app/src/main/java/com/dsyu/stormy/MainActivity.java
@@ -0,0 +1,67 @@
+package com.dsyu.stormy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+
+        String apiKEY = "e17fcb932a85d49efc27e7e843e22a46";
+
+        double latitude = 37.8267;
+        double longitude = -122.4233;
+
+        String forecastURL = "https://api.darksky.net/forecast/" + apiKEY + "/" + latitude + "," + longitude;
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder().url(forecastURL).build();
+
+        Call call = client.newCall( request );
+        call.enqueue( new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    Log.v(TAG, response.body().string());
+                    if (response.isSuccessful()) {
+
+                    }
+                    else {
+                        alertUserAboutError();
+                    }
+                } catch (IOException e) {
+                    Log.e(TAG, "IO Exception caught", e);
+                }
+
+            }
+        } );
+
+
+
+    }
+
+    private void alertUserAboutError() {
+        AlertDialogFragment dialog = new AlertDialogFragment();
+        dialog.show(getSupportFragmentManager(), "error_dialog");
+    }
+}
